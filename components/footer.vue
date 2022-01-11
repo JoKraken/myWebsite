cf1546a5d393f9cf9b45b1bea58e3c365bc9ddbc
JoKra <Jo765130@gmail.com>
2022-01-11 23:27:32 +0100

    edit footer

---
+++ components/footer.vue
@@ -0,0 +1,63 @@
+<template>
+    <footer class="footer">
+        <ul class="footer_list">
+            <li>
+                <p>Johanna Kraken</p>
+                <p>Franz-Sellhorst-Str. 20,<br> 48432 Rheine</p>
+                <p>Email: J.Kraken24@hotmail.com</p>
+            </li>
+            <li>
+                <p>Pages:</p>
+                <p><a href="#">Home</a></p>
+                <p><a href="#">Portfolio</a></p>
+                <p><a href="#">Contact</a></p>
+            </li>
+            <li>
+                <p>Links:</p>
+                <p><a href="#">Github</a></p>
+                <p><a href="#">LinkedIn</a></p>
+                <p><a href="#">XING</a></p>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<style>
+    body {
+        margin: 0;
+    }
+
+    .footer {
+        background-color: brown;
+        padding: 15px;
+        display: flex;
+        flex-flow: row;
+        color: white;
+        justify-content: space-between
+
+
+    }
+    
+    .footer__list {
+        display: flex;
+        flex-flow: row;
+
+    }
+    
+    .footer__list > li {
+        margin: 0 15px;
+    }
+
+    .footer__list > li > a {
+        text-decoration: none;
+        color: white;
+    }
+
+    .footer__list > li::marker {
+        content: none;
+    }
+
+    .footer__list > li > a:hover {
+        text-decoration: underline;
+    }
+</style>
