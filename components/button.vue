200c9036af7f056be81f83ce0ff41c001079d005
JoKra <Jo765130@gmail.com>
2022-01-28 18:49:33 +0100

    add button

---
+++ components/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <a class="button" :href="data.link" target="_blank">
+    {{data.name}}
+  </a>
+</template>
+
+<script>
+  export default {
+      props: ['data'],
+      data() {
+          return {};
+      }
+  }
+</script>
+             
+<style lang="scss" scoped>
+    .button {
+        background-color: $primary;
+        border: $primary 1px solid;
+        border-radius: 5px;
+        color: $white;
+        padding: 10px;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+</style>
