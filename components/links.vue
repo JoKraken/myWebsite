f4463c5934c08e959786155bd11b5f583e1436e1
JoKra <Jo765130@gmail.com>
2022-01-28 18:49:42 +0100

    add links

---
+++ components/links.vue
@@ -0,0 +1,21 @@
+<template>
+  <a class="links" :href="data.link">
+    <img :if="data.name" :src="'../assets/icons/'+data.name+'.png'">
+    <span>{{data.url}}</span>
+  </a>
+</template>
+
+<script>
+  export default {
+      props: ['data'],
+      data() {
+          return {};
+      }
+  }
+</script>
+
+<style lang="scss" scoped>
+    a > span:hover {
+        text-decoration: underline;
+    }
+</style>
